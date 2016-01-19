2e35eff68743c957bed0f520103fc6ee46daf1e3
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-01-19 09:51:55 +0100

    reversed color coding of trend in tooltip

--- core.lua
+++ core.lua
@@ -53,9 +53,9 @@ function Aux_OnLoad()
                 else
                     market_value_line = 'Market Value: '..Aux.util.money_string(market_value * count)..' / '..Aux.util.money_string(market_value)
                     if trend_percentage > 0 then
-                        market_value_line = market_value_line..' ('..GREEN_FONT_COLOR_CODE..'+'..trend_percentage..'%'..FONT_COLOR_CODE_CLOSE..')'
+                        market_value_line = market_value_line..' ('..RED_FONT_COLOR_CODE..'+'..trend_percentage..'%'..FONT_COLOR_CODE_CLOSE..')'
                     elseif trend_percentage < 0 then
-                        market_value_line = market_value_line..' ('..RED_FONT_COLOR_CODE..trend_percentage..'%'..FONT_COLOR_CODE_CLOSE..')'
+                        market_value_line = market_value_line..' ('..GREEN_FONT_COLOR_CODE..trend_percentage..'%'..FONT_COLOR_CODE_CLOSE..')'
                     end
                 end
                 EnhTooltip.AddLine(market_value_line, nil, true)
