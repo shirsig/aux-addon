8217f6327b2bebc5a43f5f3480e77301070ebd52
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-07-16 00:06:37 +0200

    version bump after giant refactoring

--- core.lua
+++ core.lua
@@ -110,7 +110,7 @@ end
 private.active_tab = nil
 
 function public.on_load()
-    public.version = '2.16.2'
+    public.version = '3.0.0'
     public.blizzard_ui_shown = false
     public.bids_loaded = false
     public.current_owner_page = nil
