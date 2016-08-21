c235f0332e95e8e143726a19324e99d5242d6c96
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-08-21 21:28:50 +0200

    big refactoring

--- module.lua
+++ module.lua
@@ -1,8 +1,8 @@
 local type, setmetatable, setfenv, unpack, mask, _g = type, setmetatable, setfenv, unpack, bit.band, getfenv(0)
-local PRIVATE, PUBLIC, MUTABLE, PROPERTY = 0, 1, 2, 4
-local MODIFIER = {private=PRIVATE, public=PUBLIC, mutable=MUTABLE, property=PROPERTY}
-local MODIFIER_MASK, PROPERTY_MASK = {private=MUTABLE+PROPERTY, public=MUTABLE+PROPERTY, mutable=PRIVATE+PUBLIC}, PRIVATE+PUBLIC
-local error, declaration_error, immutable_error, collision_error, void_error, import, property_setter, lock_mt, env_mt, interface_mt, declarator_mt
+local PRIVATE, PUBLIC, MUTABLE, PROPERTY, GETTER, SETTER = 0, 1, 2, 4, 8, 16
+local MODIFIER = {private=PRIVATE, public=PUBLIC, mutable=MUTABLE, getter=GETTER+PROPERTY, setter=SETTER+PROPERTY}
+local MODIFIER_MASK, PROPERTY_MASK = {private=MUTABLE+GETTER+SETTER, public=MUTABLE+GETTER+SETTER, mutable=PRIVATE+PUBLIC, getter=PRIVATE+PUBLIC, setter=PRIVATE+PUBLIC}, PRIVATE+PUBLIC
+local error, declaration_error, immutable_error, collision_error, void_error, import, set_property, lock_mt, env_mt, interface_mt, declarator_mt
 lock_mt = {}
 local _state, _modules = {}, setmetatable({}, lock_mt)
 function error(message, level, ...) _g.error(format(message, unpack(arg))..'\n'..debugstack(3, 5, 0), (level or 1) + 1) end
@@ -11,27 +11,36 @@ function immutable_error(key, level) error('Field "%s" is immutable.', level + 1
 function collision_error(key, level) error('Field "%s" already exists.', level + 1, key) end
 function void_error(key, level) error('No field "%s".', level + 1, key) end
 function import(imports, t) for k, v in t do imports[type(k) == 'number' and v or k] = v end end
-declarator_mt = {__metatable=false }
-function property_setter(data, property, value)
-	local state = _state[self]; local property = state.property
-	if not property or data[property] then declaration_error(2) end
-	data[property] = type(value) == 'function' and value or declaration_error(2)
+function set_property(data, property, value)
+	if property and not data[property] and type(value) == 'function' or declaration_error(2) then
+		data[property] = value
+	end
 end
+declarator_mt = {__metatable=false}
 function declarator_mt.__call() end
 function declarator_mt.__index(self, key)
 	local state, modifier = _state[self], MODIFIER[key]
 	if modifier then
-		state.modifiers = modifier + mask(MODIFIER_MASK[key], state.modifiers)
+		if mask(MODIFIER_MASK[key], state.modifiers) ~= state.modifiers then declaration_error(2) end
+		state.modifiers = modifier + state.modifiers
 		return self
 	elseif not state.metadata[key] or collision_error(key, 2) then
-		state.property, state.metadata[key], state.modifiers = key, PROPERTY + mask(PROPERTY_MASK, state.modifiers), PRIVATE
-		return state.property_initializer
+		if mask(PROPERTY_MASK, state.modifiers) ~= state.modifiers then declaration_error(2) end
+		state.property, state.metadata[key], state.modifiers = key, PROPERTY + state.modifiers, PRIVATE
 	end
 end
 function declarator_mt.__newindex(self, key, value)
 	local state = _state[self]
-	if state.metadata[key] then collision_error(key, 2) end
-	state.metadata[key], state.data[key], state.modifiers = state.modifiers, value, PRIVATE
+	if not state.metadata[key] or collision_error(key, 2) then
+		state.metadata[key] = state.modifiers
+		if mask(PROPERTY, state.modifiers) == 0 then
+			state.data[key] = value
+		elseif type(value) == 'function' or declaration_error(2) then
+			if mask(GETTER, state.modifiers) ~= 0 then state.getters[key] = value else state.setters[key] = value end
+			state.property = key
+		end
+	end
+	state.modifiers = PRIVATE
 end
 do
 	local function index(access, default)
@@ -69,14 +78,15 @@ do
 end
 function _g.aux_module(name)
 	if not _modules[name] then
-		local state, imports, env, interface, declarator, property
-		imports, env, interface, declarator, property = {}, setmetatable({}, env_mt), setmetatable({}, interface_mt), setmetatable({}, declarator_mt), setmetatable({}, property_mt)
+		local state, getters, setters, imports, env, interface, declarator
+		imports, env, interface, declarator = {}, setmetatable({}, env_mt), setmetatable({}, interface_mt), setmetatable({}, declarator_mt)
+		getters = {private=function() state.modifiers = PRIVATE return declarator end, public=function() state.modifiers = PUBLIC return declarator end, mutable=function() state.modifiers = MUTABLE return declarator end}
+		setters = {getter=function(value) set_property(getters, state.property, value) end, setter=function(value) set_property(setters, state.property, value) end}
 		state = {
 			name=name, env=env, interface=interface, imports={}, declarator_state=PRIVATE,
-			metadata = setmetatable({_g=PRIVATE, _m=PRIVATE, _i=PRIVATE, import=PRIVATE, private=PROPERTY, public=PROPERTY, getter=PROPERTY, setter=PROPERTY, mutable=PROPERTY}, lock_mt),
-			data = {_g=_g, _m=env, _i=interface, property=property, import=function(t) import(imports, t) end},
-			getters = {private=function() state.modifiers = PRIVATE return declarator end, public=function() state.modifiers = PUBLIC return declarator end, mutable=function() state.modifiers = MUTABLE return declarator end},
-			setters = {},
+			metadata = setmetatable({_g=PRIVATE, _m=PRIVATE, _i=PRIVATE, import=PRIVATE, private=PROPERTY, public=PROPERTY+GETTER, mutable=PROPERTY+GETTER, getter=PROPERTY+GETTER+SETTER, setter=PROPERTY+GETTER+SETTER}, lock_mt),
+			data = {_g=_g, _m=env, _i=interface, import=function(t) import(imports, t) end},
+			getters=getters, setters=setters,
 		}
 		_modules[name], _state[env], _state[interface], _state[declarator] = state, state, state, state
 	end
