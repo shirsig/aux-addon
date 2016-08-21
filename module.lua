ba61cb2f651865c3042ce9a8ea400b90a43291fe
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-08-21 13:13:17 +0200

    big refactoring

--- module.lua
+++ module.lua
@@ -2,15 +2,10 @@ local type, setmetatable, setfenv, unpack, mask, g = type, setmetatable, setfenv
 local PRIVATE, PUBLIC, MUTABLE, PROPERTY = 0, 1, 2, 4
 local MODIFIER = {private=PRIVATE, public=PUBLIC, mutable=MUTABLE, property=PROPERTY}
 local MODIFIER_MASK, PROPERTY_MASK = {private=MUTABLE+PROPERTY, public=MUTABLE+PROPERTY, mutable=PRIVATE+PUBLIC}, PRIVATE+PUBLIC
-local import, error, define_property, modules_mt, module_mt, env_mt, interface_mt, declarator_mt
-local _state, _lock_metatables = {}, {}
+local import, error, lock_mt, env_mt, interface_mt, declarator_mt
+local _state, _modules = {}, {}
 function error(message, ...) g.error(format(message, unpack(arg))..'\n'..debugstack(3, 5, 0), 3) end
 function import(imports, t) for k, v in t do imports[type(k) == 'number' and v or k] = v end end
---function lock_metatable()
---	local mt = {__metatable=false}
---	tinsert(mt, _lock_metatables)
---	return mt
---end
 declarator_mt = {__metatable=false}
 function declarator_mt.__call(self, t)
 	local state = _state[self]; local key = state.property
@@ -73,7 +68,7 @@ do
 	function interface_mt.__newindex(self, key, value)
 		local state = _state[self]
 		if mask(PUBLIC+PROPERTY, state.metadata[key]) == PUBLIC+PROPERTY then
-			return state.setters[key][key](value)
+			return state.setters[key](value)
 		elseif mask(PUBLIC+MUTABLE, state.metadata[key]) == PUBLIC+MUTABLE or error('Field "%s" is immutable.', key) then
 			state.data[key] = value
 		end
@@ -85,16 +80,12 @@ function g.aux_module(name)
 		env, interface, declarator, imports = setmetatable({}, env_mt), setmetatable({}, interface_mt), setmetatable({}, declarator_mt), {}
 		state = {
 			name = name, env = env, interface = interface, declarator = declarator, imports = {}, declarator_state = PRIVATE,
-			metadata={_g=PRIVATE, _m=PRIVATE, _i=PRIVATE, import=PRIVATE, private=GETTER, public=GETTER, getter=GETTER, setter=GETTER, mutable=GETTER},
+			metadata={_g=PRIVATE, _m=PRIVATE, _i=PRIVATE, import=PRIVATE, private=PROPERTY, public=PROPERTY, getter=PROPERTY, setter=PROPERTY, mutable=PROPERTY},
 			data = {_g=g, _m=env, _i=interface, import=function(t) import(imports, t) end},
-			getters = {
-				private=function() state.modifiers = PRIVATE return declarator end, public=function() state.modifiers = PUBLIC return declarator end,
-				getter=function() state.modifiers = GETTER return declarator end, setter=function() state.modifiers = SETTER return declarator end,
-				mutable=function() state.modifiers = MUTABLE return declarator end,
-			},
+			getters = {private=function() state.modifiers = PRIVATE return declarator end, public=function() state.modifiers = PUBLIC return declarator end, mutable=function() state.modifiers = MUTABLE return declarator end},
 			setters = {},
 		}
-		_state[name], _state[env], _state[interface], _state[declarator] = state, state, state, state
+		_modules[name], _state[env], _state[interface], _state[declarator] = state, state, state, state
 	end
 	local state = (_state[name])
 	setfenv(2, state.env)
@@ -106,24 +97,21 @@ frame:SetScript('OnEvent', function()
 	lock_mt.__newindex = function() error 'Cannot change modules after the loading phase.' end
 	local count = 0
 	local t0 = GetTime()
-	for name, module in _state do
-		local data, metadata, getters, setters = module.data, module.metadata, module.getters, module.setters
-		for alias, name in _imports[name] do
-			local import = _state[name]
-			if not import then error('Import failed. No module "%s".', name) end
-			local import_data, import_getters, import_setters = import.data
+	for _, state in _state do
+		local metadata, data, getters, setters = state.metadata, state.data, state.getters, state.setters
+		for alias, name in state.imports do
+			local state = _state[name]
+			if not state then error('Import failed. No module "%s".', name) end
+			local import_data, import_getters, import_setters = state.data, state.getters, state.setters
 			if alias == '' then
 				for key, modifiers in import.metadata do
-					if mask(PUBLIC, modifiers) ~= 0 then
-						if mask(GETTER+SETTER, modifiers) ~= 0 then
-
-						count = count + 1
-						_declarator_state[declarator] = modifiers
-						declarator[key] = data[key]
-					end
+					if metadata[key] then error('') end
+					count = count + 1
+					metadata[key], data[key], getters[key], setters[key] = modifiers, import_data[key], import_getters[key], import_setters[key]
 				end
 			else
-				declarator[alias] = module.interface
+				if metadata[alias] then error('') end
+				metadata[alias], data[alias] = PRIVATE, module.interface
 			end
 		end
 	end
