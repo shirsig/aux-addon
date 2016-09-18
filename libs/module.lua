58cdf62bcfd86c0d199d7950143cb5318c507810
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-09-18 18:56:36 +0200

    refactoring

--- libs/module.lua
+++ libs/module.lua
@@ -27,22 +27,22 @@ function definition_helper_mt:__index(k)
 	return self
 end
 function definition_helper_mt:__newindex(k, v) local module, modifiers = _module[self], _modifiers[self]
-	local access = ACCESS[tremove(modifiers, 1)] or error('Invalid definition.')
-	local name = META[k] and (tremove(modifiers) or error('Invalid definition.')) or k
-	if type(name) ~= 'string' or not strfind(name, '^[_%a][_%w]*') then error('Invalid definition.') end
+	local access = ACCESS[tremove(modifiers, 1)] or error'Invalid definition.'
+	local name = META[k] and (tremove(modifiers) or error'Invalid definition.') or k
+	if type(name) ~= 'string' or not strfind(name, '^[_%a][_%w]*') then error'Invalid definition.' end
 	local type = META[k] or FIELD
-	module.defined[name..OPERATION[type]] = module.defined[name..OPERATION[type]] and error('"%s" already exists.', name) or true
+	module.defined[name .. OPERATION[type]] = module.defined[name .. OPERATION[type]] and error('"%s" already exists.', name) or true
 	for i = getn(modifiers), 1, -1 do v = module[FIELD][modifiers[i]](v) end
 	module[type][name], module[access+type][name] = v, v
 	setn(modifiers, 0)
 end
 
 local function import(self, interface)
-	local module = (interface == INTERFACE or error('Import error.')) and _module[interface]
+	local module = (interface == INTERFACE or error'Import error.') and _module[interface]
 	for _, type in TYPES do
 		for k, v in module[PUBLIC+type] do
-			if not self.defined[k..OPERATION[type]] then
-				self.defined[k..OPERATION[type]], self[type][k] = true, v
+			if not self.defined[k .. OPERATION[type]] then
+				self.defined[k .. OPERATION[type]], self[type][k] = true, v
 			end
 		end
 	end
@@ -56,7 +56,7 @@ function global_mt:__index(key)
 	local module, environment, interface, definition_helper, accessors, mutators, fields, public_accessors, public_mutators, public_fields
 	environment, interface, definition_helper = {}, {}, setmetatable({}, definition_helper_mt)
 	accessors = { private=function() return definition_helper.private end, public=function() return definition_helper.public end }
-	mutators = setmetatable({_=nop}, { __index=function(_, k) return function(v) if v == interface and (_G[k] == nil or _G.error(nil)) then _G[k] = v else definition_helper.private[k] = v end end end})
+	mutators = setmetatable({ _=nop }, { __index=function(_, k) return function(v) if v == interface and (_G[k] == nil or _G.error(nil)) then _G[k] = v else definition_helper.private[k] = v end end end })
 	fields = setmetatable(
 		{ _E=environment, _I=interface, _G=_G, import=function(interface) import(module, interface) end, error=error, nop=nop, id=id },
 		{ __index=function(_, key) local accessor = accessors[key]; if accessor then return accessor() else return _G[key] end end }
