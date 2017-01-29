272aad034b013eab5513d9002bee2bfd99a744e1
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-01-29 17:04:50 +0100

    auctionable test improvement

--- util/info.lua
+++ util/info.lua
@@ -263,12 +263,13 @@ function M.display_name(item_id, no_brackets, no_color)
 end
 
 function M.auctionable(tooltip, quality, lootable)
+    local status = tooltip[2] and tooltip[2].left_text
     local durability, max_durability = durability(tooltip)
     return not lootable
             and (not quality or quality < 6)
-            and not tooltip_match(ITEM_BIND_ON_PICKUP, tooltip)
-            and not tooltip_match(ITEM_BIND_QUEST, tooltip)
-            and not tooltip_match(ITEM_SOULBOUND, tooltip)
+            and status ~= ITEM_BIND_ON_PICKUP
+            and status ~= ITEM_BIND_QUEST
+            and status ~= ITEM_SOULBOUND
             and (not tooltip_match(ITEM_CONJURED, tooltip) or tooltip_find(ITEM_MIN_LEVEL, tooltip) > 1)
             and not (durability and durability < max_durability)
 end
