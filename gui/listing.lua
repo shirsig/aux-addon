d03e945dff45469897bb841e15ba87fa823fa6b8
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-02-09 11:26:49 +0100

    consistent highlight color

--- gui/listing.lua
+++ gui/listing.lua
@@ -246,7 +246,7 @@ local methods = {
         end
         local highlight = row:CreateTexture()
         highlight:SetAllPoints()
-        highlight:SetTexture(1, .9, 0, .3)
+        highlight:SetTexture(1, .9, 0, .4)
         highlight:Hide()
         row.highlight = highlight
         row.st = self
