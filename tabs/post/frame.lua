d6fefc5184fd185b43e4cd7c0565d2c43d5e1ae8
Simon Hirsig <simon.hirsig@gmail.com>
2018-09-22 11:02:53 +0200

    added back right click for deselecting in the post listing

--- tabs/post/frame.lua
+++ tabs/post/frame.lua
@@ -85,7 +85,11 @@ bid_listing:SetSelection(function(data)
 	return data.record == get_bid_selection() or data.record.historical_value and get_bid_selection() and get_bid_selection().historical_value
 end)
 bid_listing:SetHandler('OnClick', function(table, row_data, column, button)
-    set_bid_selection(row_data.record)
+	if button == 'RightButton' and row_data.record == get_bid_selection() or row_data.record.historical_value and get_bid_selection() and get_bid_selection().historical_value then
+		set_bid_selection()
+	else
+		set_bid_selection(row_data.record)
+	end
 	refresh = true
 end)
 bid_listing:SetHandler('OnDoubleClick', function(table, row_data, column, button)
@@ -105,7 +109,11 @@ buyout_listing:SetSelection(function(data)
 	return data.record == get_buyout_selection() or data.record.historical_value and get_buyout_selection() and get_buyout_selection().historical_value
 end)
 buyout_listing:SetHandler('OnClick', function(table, row_data, column, button)
-    set_buyout_selection(row_data.record)
+	if button == 'RightButton' and row_data.record == get_buyout_selection() or row_data.record.historical_value and get_buyout_selection() and get_buyout_selection().historical_value then
+		set_buyout_selection()
+	else
+		set_buyout_selection(row_data.record)
+	end
 	refresh = true
 end)
 buyout_listing:SetHandler('OnDoubleClick', function(table, row_data, column, button)
