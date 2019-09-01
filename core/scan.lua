e3f27b62617d209e2fa7bf43431064abec8abd41
Simon Hirsig <simon.hirsig@gmail.com>
2019-09-01 13:45:25 +0200

    bugfix

--- core/scan.lua
+++ core/scan.lua
@@ -104,7 +104,6 @@ do
         SortAuctionClearSort(get_state().params.type)
         SortAuctionItems(get_state().params.type, 'duration')
         SortAuctionItems(get_state().params.type, 'duration')
-        SortAuctionApplySort(get_state().params.type)
 		if get_state().params.type == 'bidder' and not AuctionFrame.gotBids then
             GetBidderAuctionItems()
             AuctionFrame.gotBids = 1
