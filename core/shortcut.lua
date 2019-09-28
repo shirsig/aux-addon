28ee40f386dc20a511f45659093d9cf50be8328a
Simon Hirsig <simon.hirsig@gmail.com>
2019-09-29 00:49:10 +0200

    bag item alt click fix

--- core/shortcut.lua
+++ core/shortcut.lua
@@ -3,8 +3,8 @@ select(2, ...) 'aux.core.shortcut'
 local aux = require 'aux'
 local info = require 'aux.util.info'
 
---hooksecurefunc('HandleModifiedItemClick', function(...)
---    if IsAltKeyDown() and aux.index(aux.get_tab(), 'USE_ITEM') then
---        aux.get_tab().USE_ITEM(info.parse_link(select(1, ...)))
---    end
---end)
+hooksecurefunc('HandleModifiedItemClick', function(...)
+    if IsAltKeyDown() and aux.index(aux.get_tab(), 'USE_ITEM') then
+        aux.get_tab().USE_ITEM(info.parse_link(select(1, ...)))
+    end
+end)
