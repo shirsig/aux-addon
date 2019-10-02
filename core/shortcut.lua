7d096c0439cb76430e6d3b54746cd015bd97b4b3
Simon Hirsig <simon.hirsig@gmail.com>
2019-10-02 21:54:50 +0200

    alt-click fix

--- core/shortcut.lua
+++ core/shortcut.lua
@@ -3,8 +3,8 @@ select(2, ...) 'aux.core.shortcut'
 local aux = require 'aux'
 local info = require 'aux.util.info'
 
-hooksecurefunc('HandleModifiedItemClick', function(...)
-    if IsAltKeyDown() and aux.index(aux.get_tab(), 'USE_ITEM') then
-        aux.get_tab().USE_ITEM(info.parse_link(select(1, ...)))
+hooksecurefunc('HandleModifiedItemClick', function(item_link)
+    if item_link and IsAltKeyDown() and aux.index(aux.get_tab(), 'USE_ITEM') then
+        aux.get_tab().USE_ITEM(info.parse_link(item_link))
     end
 end)
