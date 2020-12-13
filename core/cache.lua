676c0540dd44cf6e8b0b33530aad1cae9ebd5d3f
Simon Hirsig <simon.hirsig@gmail.com>
2020-12-13 16:02:32 +0100

    search slowness bugfix

--- core/cache.lua
+++ core/cache.lua
@@ -169,7 +169,11 @@ end
 
 function on_get_item_info_received(item_id, success)
     if success then
-        aux.coro_thread(function() process_item(item_id) end)
+        aux.coro_thread(function()
+            if not aux.account_data.items[item_id] and not aux.account_data.unused_item_ids[item_id] then
+                process_item(item_id)
+            end
+        end)
     else -- TODO if success == nil then
         aux.account_data.unused_item_ids[item_id] = true
     end
